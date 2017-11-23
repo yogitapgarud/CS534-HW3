4d4c26cffde1213d33e960ffb294855c0db24669
Yogita Garud <garudyogitap@gmail.com>
2017-11-22 21:49:54 -0800

    For best error rate 4.68

--- Predictor.py
+++ Predictor.py
@@ -1,22 +1,68 @@
 import numpy as np
+from tagger import decode
+from tagger import decodetrigram1
+from trainer import avgPerceptron
+from tagger import mle
+from tagger import readfile
+from trainer import avgPerceptronBivariant1
+from trainer import avgPerceptronTrigramFeatures
+import sys
 
-def Predictor(model, testData):
+def Predictor(model):
     with open("test.txt.lower.unk.unlabeled", "r") as f:
         output = open('test.txt.lower.unk.best.txt', 'w') 
-        i = 0
-        positiveResults = 0
 
         for line in f:
-            testRow = testData[i, 0:-1]
             
-            if np.dot(testRow.astype(int), weightVector) <= 0:
-                # <= 50
-                output.write(line.replace('\n',' <=50\n'))
-            else:
-                # >50
-                output.write(line.replace('\n',' >50\n'))
-                positiveResults += 1
-            i += 1
+            words = line.split()
+
+            #print(words)
+
+            mytags = decodetrigram1(words, dictionary , model)
+
+            words_tags = zip(words, mytags)
+
+            for w, t in words_tags:
+
+	            output.write(w)
+                    output.write("/")
+		    output.write(t)
+                    output.write(" ")  
+
+            output.write('\n')
+
         output.close()
-        
-    return positiveResults
+
+def devPredictor(model, filename):
+    with open("dev.txt.lower.unk", "r") as f:
+        output = open('dev.txt.lower.unk.best', 'w') 
+
+        for words, tags in readfile(filename):
+
+            mytags = decodetrigram1(words, dictionary , model)
+
+            words_tags = zip(words, mytags)
+
+            for w, t in words_tags:
+
+	            output.write(w)
+                    output.write("/")
+		    output.write(t)
+                    output.write(" ")  
+
+            output.write('\n')
+
+        output.close()
+
+
+if __name__ == "__main__":
+
+	trainfile, devfile, featurefile = sys.argv[1:4]
+    
+	dictionary, model = mle(trainfile)
+	#model = avgPerceptron(dictionary, model, trainfile, devfile, featurefile)
+	#model = avgPerceptronBivariant1(dictionary, model, trainfile, devfile)
+	model = avgPerceptronTrigramFeatures(dictionary, model, trainfile, devfile)
+	print(type(model))
+	Predictor(model)
+	devPredictor(model, devfile)
